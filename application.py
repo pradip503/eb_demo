fc8a9476a605a2baa0d232710896ccdaaeb97e6a
Pradip Bhattarai <bhattaraipradip66@gmail.com>
2021-02-01 15:01:05 +0545

    demo updated

--- application.py
+++ application.py
@@ -6,6 +6,6 @@ from flask import Flask, render_template
 application = Flask(__name__)
  
  
-@application.route('/v1')
+@application.route('/')
 def v1():
-   return 'Example app v1'
+   return 'Example app v1 updated'
